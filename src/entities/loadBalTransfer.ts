49a0428b3a032592ecdbf52608421e8d258f807d
SunlightLuck <sunlight.luck@gmail.com>
2023-05-10 21:00:12 -0400

    track bal transfer

--- src/entities/loadBalTransfer.ts
+++ src/entities/loadBalTransfer.ts
@@ -2,10 +2,10 @@ import { BigInt } from "@graphprotocol/graph-ts";
 import { BalTransferred } from "../../generated/schema";
 
 export function loadBalTransfer(): BalTransferred {
-  let balTransfer = BalTransferred.load("");
+  let balTransfer = BalTransferred.load("1");
 
   if (!balTransfer) {
-    balTransfer = new BalTransferred("");
+    balTransfer = new BalTransferred("1");
     balTransfer.amount = BigInt.fromI32(0);
 
     balTransfer.save();
--- src/standard_arb_erc20.ts
+++ src/standard_arb_erc20.ts
@@ -1,12 +1,27 @@
 import { Transfer as TransferEvent } from "../generated/StandardArbERC20/StandardArbERC20";
 import { loadBalTransfer } from "./entities/loadBalTransfer";
-import { BAL_REWARD_ADDRESS, MULTI_FEE_DISTRIBUTION_ADDRESS } from "./constants";
+import {
+  BAL_REWARD_ADDRESS,
+  MULTI_FEE_DISTRIBUTION_ADDRESS,
+} from "./constants";
+import { NewBalTransfer } from "../generated/schema";
+import { getHistoryEntityId } from "./utils";
 
-export function handleDeposit(event: TransferEvent): void {
-  if (event.params.from === BAL_REWARD_ADDRESS && event.params.to === MULTI_FEE_DISTRIBUTION_ADDRESS) {
-      let balTransfer = loadBalTransfer();
-    
-      balTransfer.amount = balTransfer.amount.plus(event.params.value);
-      balTransfer.save();
+export function handleTransfer(event: TransferEvent): void {
+  if (
+    event.params.from.toHexString().toLowerCase() ==
+      BAL_REWARD_ADDRESS.toHexString().toLowerCase() &&
+    event.params.to.toHexString().toLowerCase() ==
+    MULTI_FEE_DISTRIBUTION_ADDRESS.toHexString().toLowerCase()
+  ) {
+    let balTransferred = loadBalTransfer();
+    let newBalTransfer = new NewBalTransfer(getHistoryEntityId(event));
+    newBalTransfer.amount = event.params.value;
+    newBalTransfer.total = balTransferred.amount;
+    newBalTransfer.timestamp = event.block.timestamp.toI32();
+    newBalTransfer.save();
+
+    balTransferred.amount = balTransferred.amount.plus(event.params.value);
+    balTransferred.save();
   }
 }
